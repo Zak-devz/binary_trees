233c7bb3c32d74823d30b37382d02034588c019a
root <zakariarassili@gmail.com>
2023-08-30 14:05:00 +0000

    task 10 correct code

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,5 +10,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	else
+		return (1 + binary_tree_depth(tree->parent));
 }
